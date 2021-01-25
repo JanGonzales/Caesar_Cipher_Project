08f7d3be23d076d6e07d1194d6d53ddba2f878a2
Jan Gonzales <JanGonFlores@icloud.com>
2021-01-25 22:10:25 +0000

    .

---
+++ Functions_Inputs.py
@@ -0,0 +1,11 @@
+# Review:
+# Create a function called greet().
+# Write 3 print statements inside the function.
+# Call the greet() function and run your code.
+
+def greet():
+    print("Hell World")
+    print("Hell World 2")
+    print("Hello World for the last time I swear")
+
+greet()
