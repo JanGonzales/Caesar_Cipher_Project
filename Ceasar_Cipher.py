d0315277a24d238c05420de1b064176e18c4bcb0
Jan Gonzales <JanGonFlores@icloud.com>
2021-01-26 22:20:23 +0000

    .

--- Ceasar_Cipher.py
+++ Ceasar_Cipher.py
@@ -1,7 +1,7 @@
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u',
             'v', 'w', 'x', 'y', 'z']
 
-direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n").lower()
 text = input("Type your message:\n").lower()
 shift = int(input("Type the shift number:\n"))
 
@@ -10,8 +10,8 @@ def encrypt(plain_text, plain_shift):
     for letter in plain_text:
         if letter == " ":
             new_list += letter
-        elif alphabet.index(letter) + plain_shift > len(alphabet) -1:
-            a = len(alphabet) - 1
+        elif alphabet.index(letter) + plain_shift > len(alphabet) - 1:
+            a = len(alphabet)
             x = (alphabet.index(letter) + plain_shift) - a
             new_list += alphabet[x]
         else:
@@ -19,14 +19,21 @@ def encrypt(plain_text, plain_shift):
             new_list += alphabet[letter_number]
     print(f"The encoded text is {new_list}")
 
-# def encrypt(plain_text, plain_shift): ALTERNATIVE METHOD
-#     counter = 0
-#     new_list = ""
-#     while len(new_list)< len(text):
-#         for var in range(0, len(alphabet) -1):
-#             if text[counter] in alphabet[var]:
-#                 var += shift
-#                 new_list += alphabet[var]
-#         counter += 1
+def decrypt(plain_text, plain_shift):
+    new_list = ""
+    for letter in plain_text:
+        if letter == " ":
+            new_list += letter
+        elif alphabet.index(letter) + plain_shift < 0:
+            a = len(alphabet)
+            x = plain_shift - (alphabet.index(letter) - a)
+            new_list += alphabet[x]
+        else:
+            letter_number = (alphabet.index(letter) - plain_shift)
+            new_list += alphabet[letter_number]
+    print(f"The encoded text is {new_list}")
 
-encrypt(plain_text=text, plain_shift=shift)
+if direction == "encode":
+    encrypt(plain_text=text, plain_shift=shift)
+elif direction == "decode":
+    decrypt(plain_text=text, plain_shift=shift)
