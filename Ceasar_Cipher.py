7b15c03a974512b9d5d1d1feb1e92e3227ef1858
Jan Gonzales <JanGonFlores@icloud.com>
2021-01-26 20:59:33 +0000

    .

--- Ceasar_Cipher.py
+++ Ceasar_Cipher.py
@@ -9,14 +9,10 @@ shift = int(input("Type the shift number:\n"))
 # TODO-1: Create a function called 'encrypt' that takes the 'text' and 'shift' as inputs.
 def encrypt(plain_text, plain_shift):
     # TODO-2: Inside the 'encrypt' function, shift each letter of the 'text' forwards in the alphabet by the shift amount and print the encrypted text.
-    counter = 0
     new_list = ""
-    while len(new_list)< len(text):
-        for var in range(0, len(alphabet) -1):
-            if text[counter] in alphabet[var]:
-                var += shift
-                new_list += alphabet[var]
-        counter += 1
+    for letter in plain_text:
+        letter_number = alphabet.index(letter) + plain_shift
+        new_list += alphabet[letter_number]
     print(new_list)
     # e.g.
     # plain_text = "hello"
@@ -29,6 +25,15 @@ def encrypt(plain_text, plain_shift):
 
     ##🐛Bug alert: What happens if you try to encode the word 'civilization'?🐛
 
+# def encrypt(plain_text, plain_shift): ALTERNATIVE METHOD
+#     counter = 0
+#     new_list = ""
+#     while len(new_list)< len(text):
+#         for var in range(0, len(alphabet) -1):
+#             if text[counter] in alphabet[var]:
+#                 var += shift
+#                 new_list += alphabet[var]
+#         counter += 1
 
 # TODO-3: Call the encrypt function and pass in the user inputs. You should be able to test the code and encrypt a message.
 encrypt(plain_text=text, plain_shift=shift)
