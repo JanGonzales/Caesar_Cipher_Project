42742bdccb201b832fc636b0e39c9cee39ad898d
Jan Gonzales <JanGonFlores@icloud.com>
2021-01-27 00:41:03 +0000

    Caesar_Cipher Project Completed

--- Ceasar_Cipher.py
+++ Ceasar_Cipher.py
@@ -1,14 +1,20 @@
+import ACII_art
+import os
+
+print(ACII_art.logo)
+
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u',
             'v', 'w', 'x', 'y', 'z']
 
-direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n").lower()
-text = input("Type your message:\n").lower()
-shift = int(input("Type the shift number:\n"))
 
-def encrypt(plain_text, plain_shift):
+def caesar(plain_text, plain_shift, plain_direction):
     new_list = ""
+    if plain_shift > 26:
+        plain_shift %= 26
+    if plain_direction == "decode":
+        plain_shift *= -1
     for letter in plain_text:
-        if letter == " ":
+        if letter not in alphabet:
             new_list += letter
         elif alphabet.index(letter) + plain_shift > len(alphabet) - 1:
             a = len(alphabet)
@@ -19,21 +25,20 @@ def encrypt(plain_text, plain_shift):
             new_list += alphabet[letter_number]
     print(f"The encoded text is {new_list}")
 
-def decrypt(plain_text, plain_shift):
-    new_list = ""
-    for letter in plain_text:
-        if letter == " ":
-            new_list += letter
-        elif alphabet.index(letter) + plain_shift < 0:
-            a = len(alphabet)
-            x = plain_shift - (alphabet.index(letter) - a)
-            new_list += alphabet[x]
-        else:
-            letter_number = (alphabet.index(letter) - plain_shift)
-            new_list += alphabet[letter_number]
-    print(f"The encoded text is {new_list}")
 
-if direction == "encode":
-    encrypt(plain_text=text, plain_shift=shift)
-elif direction == "decode":
-    decrypt(plain_text=text, plain_shift=shift)
+restart = True
+while restart is True:
+    direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n").lower()
+    text = input("Type your message:\n").lower()
+    shift = int(input("Type the shift number:\n"))
+    caesar(plain_text=text, plain_shift=shift, plain_direction=direction)
+    print("Please make sure to remember your password and shift number before quitting and restarting")
+    user_restart = input("Do you want to try again y/N: ").lower()
+    if user_restart == "y":
+        if os.name == 'nt':
+            os.system('cls')
+        else:
+            os.system('clear')
+    elif user_restart == "n":
+        print("Good Bye!")
+        x = False
